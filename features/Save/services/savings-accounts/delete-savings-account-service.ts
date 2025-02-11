fa5feb6f61f47f42f97d7eec6cae8fd914694464
Andrew Chalupa <andrewchalupa@pm.me>
2025-02-11 15:37:11 -0500

    removed duplicate folders

--- features/Save/services/savings-accounts/delete-savings-account-service.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/utils/supabase/client';
-
-export const deleteSavingsAccountService = async (id: any) => {
-    const supabase = createClient();
-
-    const { error } = await supabase.from('savings_accounts').delete().eq('id', id);
-    if (error) console.log('Error deleting savings account: ', error.message);
-};
